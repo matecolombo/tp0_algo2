992f674cc7c554442f74bb5b6b115478f40e5188
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2020-05-22 22:01:37 -0300

    Update salida.cpp

--- salida.cpp
+++ salida.cpp
@@ -1,11 +1,11 @@
 
-void imagen::imprimirImagen(ofstream &ofs, Matrix<double> & mi){
+void imagen::imprimirImagen(ofstream &ofs){
 	ofs << formato << endl;
 	ofs << x" " << y << endl;
 	cout << n << endl;
 
-	for(size_t i = 0; i < mi.get_alto(); i++){
-		for(size_t j = 0; j < mi.get_ancho(); j++){
+	for(size_t i = 0; i < x; i++){
+		for(size_t j = 0; j < y; j++){
 			ofs << m.get_val_xpos(i,j) << " ";
 		}
 		ofs << endl
